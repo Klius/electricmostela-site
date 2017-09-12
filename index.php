f0f38ca187f01ed6c8b52301f152ac7dc8d7da4a
klius <kliusescarlopini@gmail.com>
2017-09-13 00:35:09 +0200

    New site is up kinda

--- index.php
+++ index.php
@@ -8,6 +8,27 @@
 	</head>
 	<body>
 		<?php include_once('header.php');?>
-		
+		<div class="center">
+			<div class="content">
+				<div class="block">
+					<div class="info">
+						<h3>Smoked Sardines (2016)</h3>
+						<p>
+							Smoked Sardines es un joc competitiu per dos persones.
+							Com a sardina has d'evitar que el chef espacial et fregeixi amb la terrible FLAMEJATOR-300.
+							</br>Esta fet amb javascript+canvas i ho pots jugar a la seva web!
+							</br>
+							</br>
+							<a href="https://klius.github.io/Smoked-Sardines">
+								<img src="img/logos/GitHub-Mark-64px.png"/>
+							</a>
+						</p>
+					</div>
+					<div class="picture">
+						<img src="img/smokedsardines.png"/>
+					</div>
+				</div>
+			</div>
+		</div>
 	</body>	
 </html>

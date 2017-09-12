e7195f23bb60e14e777531c889472c9d54e12a2f
Victor Asensio <GUARDIANH2@GUARDIAN.local>
2017-09-12 17:42:35 +0200

    Defined the header of the site

---
+++ header.php
@@ -0,0 +1,16 @@
+<div id="header">
+	<div class="centerHeader">
+		<div class="headerTitle">
+			<h1 class="title">Electric Mostela</h1>
+			<p class="subtitle">I'm puto loco</p>
+		</div>
+		<div class="headerNavigation">
+			<ul>
+				<li><a href="#">About</a></li>
+				<li><a href="index.php">Games</a></li>
+				<li><a href="#">Music</a></li>
+				<li><a href="#">Coding</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>Electric Mostela</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="shortcut icon" href="/favicon.gif" type="image/gif" />
+		<link rel="stylesheet" href="css/styletto.css" type="text/css"/>
+	</head>
+	<body>
+		<?php include_once('header.php');?>
+		
+	</body>	
+</html>

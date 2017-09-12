9099bb3952ece2ac24599984eeab965b533d4aba
klius <kliusescarlopini@gmail.com>
2017-09-13 00:37:03 +0200

    changed the motto

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<div class="centerHeader">
 		<div class="headerTitle">
 			<h1 class="title">Electric Mostela</h1>
-			<p class="subtitle">I'm puto loco</p>
+			<p class="subtitle">I make things and stuff</p>
 		</div>
 		<div class="headerNavigation">
 			<ul>

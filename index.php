f31d5f79e80f284825c8c2c42d53fcbc7b736189
Klius <Klius@users.noreply.github.com>
2017-09-13 01:22:20 +0200

    removed viewport adjust
    only until responsive css is made

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<title>Electric Mostela</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="shortcut icon" href="/favicon.gif" type="image/gif" />
+       <!-- <meta name="viewport" content="width=device-width, initial-scale=1" />
+	-->	<link rel="shortcut icon" href="/favicon.gif" type="image/gif" />
 		<link rel="stylesheet" href="css/styletto.css" type="text/css"/>
 	</head>
 	<body>
